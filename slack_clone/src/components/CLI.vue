03a9bf34d3771c84923c2f8370df2feb49b6c32c
Matej Kotuc <xmatokx@gmail.com>
2025-10-09 17:41:56 +0200

    functioning cli

---
+++ slack_clone/src/components/CLI.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-input outlined v-model="messageInput"
+    class="input-field full-height full-width"
+    @keyup.enter.exact.prevent="submitMessage"
+  />
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      messageInput: '',
+    };
+  },
+  methods: {
+    submitMessage() {
+      const mess = this.messageInput.trim();
+      if (!mess) return;
+      this.$emit('submitMessageEvent', mess);
+      this.messageInput=''
+    }
+  },
+  emits: {
+    submitMessageEvent: (msg: string) => typeof msg === 'string'
+  },
+}
+</script>
+
+<style scoped>
+  input.input-field {
+    border-radius: 20px;
+  }
+</style>
