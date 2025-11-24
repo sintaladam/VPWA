dcd3c7bbe0208327526abd3d6ddc438e66174d60
adam <adamsintal3@gmail.com>
2025-11-24 15:10:43 +0100

    :(i dont know what happened

--- slack_clone/src/components/MessageBoard.vue
+++ slack_clone/src/components/MessageBoard.vue
@@ -8,13 +8,6 @@
           </div>
         </template>
 
-      <q-infinite-scroll :offset="250" @load="onLoad" class="column" reverse>
-        <template v-slot:loading>
-          <div class="row justify-center q-my-md">
-            <q-spinner-dots color="primary" size="40px" />
-          </div>
-        </template>
-
         <template v-for="(mess, index) in messages" :key="index">
           <q-chat-message :text="[mess.content]" :sent="mess.sender.id === userStore.user?.id"
             :name="mess.sender.nickname"
